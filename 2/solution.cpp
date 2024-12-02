a59502a7a215c5dd5024092eabc7ee06adaddab1
Edwin Harmata <edwin.harmata@gmail.com>
2024-12-02 19:42:16 +0100

    Prepare solution for first puzzle of second day

--- 2/solution.cpp
+++ 2/solution.cpp
@@ -1,65 +1,101 @@
+#include <stdexcept>
 #include <vector>
 #include <iostream>
 #include <fstream>
 #include <string>
 
+#define DEFAULT_INPUT_NAME  "2/input.txt"
+
 bool isSafe(std::vector<int> &numbers) {
-    bool isIncreasig = numbers[0] < numbers[1];
     int diff = 0;
 
+    // determine if numbers are increasing or decreasing based on two first positions in our vector
+    bool isIncreasig = numbers[0] < numbers[1];
+
+    // we loop over all given numbers
     for(int i = 1; i < numbers.size(); i++) {
+        // verify if any pair of following numbers broke business rules
         if(isIncreasig) {
             diff = numbers[i] - numbers[i-1];
-            return diff > 0 && diff < 4;
+            if(diff <= 0 || diff > 3) {
+                return false;
+            }
         } else if(!isIncreasig) {
             diff = numbers[i-1] - numbers[i];
-            return diff > 0 && diff < 4;
+            if(diff <= 0 || diff > 3) {
+                return false;
+            }
         }
     }
 
-    // sprawdzić wersję z auto wraz ze zmienną pomocniczą na poprzednią wartość (logika od indexu 1)
+    // if all given numbers meets business criteria we are returning information that all row is safe
+    return true;
+
+    // TODO: verify version with "auto" and helper variable for previous value (logic from index 1)
 }
 
-std::vector<std::vector<int>> getDataFromFile(const std::string &filename)
+void countSafeRowsInFile(const std::string &filename, int & safeRowsCount)
 {
+    // Open file to read data
     std::ifstream file(filename);
-    char currentLine[64];
-    char schowek[64];
 
+    // Prepare buffer for each line
+    char currentLine[64];
+
+    // Ensure we have access to file with data
     if (!file.is_open())
     {
-        std::cerr << "Error: Unable to open file " << filename << std::endl;
-        return;
+        throw std::runtime_error("Error: Unable to open file " + filename);
     }
 
+    // Read file line by line
+    while(file.getline(currentLine, 64)) {
 
-
-    std::vector<bool> data;
-
-    while(file) {
-        file.getline(currentLine, 64);
         std::vector<int> numbers;
         std::string currentNumber = "";
+
+        // Read each character
         for (int i = 0; i < 64; i++) {
+            // If character is space, trasnform buffered string into number
             if (currentLine[i] == ' ') {
                 numbers.push_back(std::stoi(currentNumber));
                 currentNumber = "";
+            // If character is end of line transform buffered string into number and break loop for this line
             } else if (currentLine[i] == '\0') {
                 numbers.push_back(std::stoi(currentNumber));
                 currentNumber = "";
                 break;
+            // If character is any other char (assuming it's a number) concatenate it with previous ones
             } else {
                currentNumber += currentLine[i];
             }
+        }
 
-            
+        // verify if those numbers meets our business requirments
+        if(isSafe(numbers)) {
+            // if so, increase counter
+            safeRowsCount++;
         }
     }
+
+    return;
 }
 
-int main()
+int main(int argc, char *argv[])
 {
-    int result = 0;
+    // Set default text input file name
+    std::string inputName = DEFAULT_INPUT_NAME;
+    int safeRowsCount = 0;
+    // Read filename from CLI argument if any passed
+    if(argc > 1) {
+        inputName = argv[1];
+    }
+
+    // Read data from file and immediately count safe rows
+    countSafeRowsInFile(inputName, safeRowsCount);
+
+    // Display result to the terminal
+    std::cout << "Safe items count: " << safeRowsCount << std::endl;
 
     return 0;
 }
